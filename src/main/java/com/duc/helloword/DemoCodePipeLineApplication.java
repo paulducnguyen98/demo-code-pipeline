277936b47fbc4bc81a15c02b2453143146691d68
paulducnguyen98 <paulducnguyen98@gmail.com>
2021-11-10 12:37:19 +0700

    demo code pipeline

---
+++ src/main/java/com/duc/helloword/DemoCodePipeLineApplication.java
@@ -0,0 +1,13 @@
+package com.duc.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodePipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodePipeLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duc/helloword/DemoCodePipeLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duc.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCodePipeLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
